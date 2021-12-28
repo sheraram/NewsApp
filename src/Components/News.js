f09c874e708138f51578409ef2f3b33efb7519ec
sheraram <sheraram998202@gmail.com>
2021-12-29 00:41:41 +0530

    Add more functionality to NewsApp

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,79 +2,68 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": "BBC Sport",
-            "title": "England never stood a chance - Agnew",
-            "description": "The structure of domestic cricket and a lack of preparation left England primed for another Ashes humbling, says Jonathan Agnew.",
-            "url": "http://www.bbc.co.uk/sport/cricket/59808295",
-            "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/3086/production/_122522421_australiacelebrate.jpg",
-            "publishedAt": "2021-12-28T09:37:24.9773291Z",
-            "content": "Realistically, England never stood a chance.\r\nLike plenty of others, I thought there were a number of factors going into this Ashes series that would level the playing field: the disrupted preparatio… [+5094 chars]"
-        },
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": "BBC Sport",
-            "title": "'Press the reset button' - how pundits reacted to England's Ashes defeat",
-            "description": "Minutes had barely passed before the fall-out from England's dismal Ashes defeat had begun, from Joe Root's future to the state of county cricket, here is the best reaction.",
-            "url": "http://www.bbc.co.uk/sport/cricket/59806990",
-            "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/81BC/production/_122521233_gettyimages-1361425448.jpg",
-            "publishedAt": "2021-12-28T04:22:25.0939018Z",
-            "content": "Minutes had barely passed before the fall-out from England's dismal Ashes defeat had begun.\r\nThere was talk of an \"embarrassing\" performance, Joe Root stepping down as captain, the future of head coa… [+6501 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-        
-    ]
+
     constructor(){
             super();
             this.state={
-                articles : this.articles,
-                loading : false
+                articles : [],
+                loading : false,
+                page: 1
             }
         }
+    
+    async componentDidMount(){
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=f7444080d87a40aca9559ffdce51da3e&page=1&pageSize=15";
+        let data = await fetch(url);
+        let datajson= await data.json();
+        console.log(datajson);
+        this.setState({articles : datajson.articles, totalResults : datajson.totalResults})
+    }
+
+    handlenextclick=async ()=>{
+        console.log("next");
+
+        
+            let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=f7444080d87a40aca9559ffdce51da3e&page=${this.state.page + 1}&pageSize=15`;
+            let data = await fetch(url);
+            let datajson= await data.json();
+            console.log(datajson);
+            this.setState({
+                page : this.state.page +1 ,
+                articles : datajson.articles
+            })
+        
+    }
+
+    handleprevclick=async ()=>{
+        console.log("previous");
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=f7444080d87a40aca9559ffdce51da3e&page=${this.state.page - 1}&pageSize=15`;
+        let data = await fetch(url);
+        let datajson= await data.json();
+        console.log(datajson);
+        this.setState({
+            page : this.state.page - 1,
+            articles : datajson.articles            
+        })
+    }
+
     render() {
+        
         return (
             <div className="container my-3">
                 <h2>Top Headlines .. </h2>
                 <div className="row">
                     {this.state.articles.map((element)=>{
                         return <div className="col-md-4" key={element.url}>
-                        <NewsItems title={element.title.slice(0,45)} description={element.description.slice(0,80)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        <NewsItems title={!element.title?"":element.title} description={element.description?element.description:""} imageUrl={!element.urlToImage?"https://fox8.com/wp-content/uploads/sites/12/2021/12/AP21300808623855.jpg?w=1280":element.urlToImage} newsUrl={element.url}/>
                     </div>
                     })}
                 </div>
+                <div className="container d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handleprevclick}> &laquo; Previous</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/15)} className="btn btn-dark" onClick={this.handlenextclick} >Next &raquo;</button>
+                </div>
+                
             </div>
         )
     }
