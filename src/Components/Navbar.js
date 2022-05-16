9a0b3e43756aaea88136f06f6dc4fb5e5985fdcb
SHERA RAM <shera@styava.com>
2022-05-17 01:41:07 +0530

    Update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,10 +6,18 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div>
-            <nav className='navbar navbar-expand-lg fixed-top navbar-light ' style={{backgroundColor: '#e3f2fd'}}>
+            <nav className='navbar navbar-expand-lg fixed-top navbar-light ' style={{ backgroundColor: '#e3f2fd' }}>
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">NewsApp</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
